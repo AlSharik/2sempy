1268513036f9630fbc731ac6fbded3cab493a228
Alexander Shar <alahan05@gmail.com>
2023-04-27 15:25:36 +0300

    10homework.py

--- 10homework.py
+++ 10homework.py
@@ -3,5 +3,11 @@ up = 0
 down = 0
 while coins != 0:
     coins-=1
-    if input("Сторона: ") == 1:
-        
+    if int(input("Сторона: ")) == 1:
+        up+=1
+    else:
+        down+=1
+if up > down:
+    print(down)
+else:
+    print(up)
